4a586131ff71d4d6afc93ebc7946aaa708c14404
berendeev <sellect.47@gmail.com>
2019-12-04 01:37:28 +0600

    added general files. added script which invoked then div message changed

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+// Select the node that will be observed for mutations
+const targetNode =  document.querySelector("._im_peer_history");
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: false, childList: false, subtree: false };
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) {
+    alert("dds");
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
